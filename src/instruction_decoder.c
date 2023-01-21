c31a138b8d2f2e44b7cf495bda771b2c126ba543
Chandler Teigen <chandlerteigen@gmail.com>
2023-01-20 22:28:31 -0800

    add decode function with NOP impelementation

---
+++ src/instruction_decoder.c
@@ -0,0 +1,21 @@
+#include "instruction_decoder.h"
+
+
+int decode(char* p_instruction)
+{
+    char opcode = *p_instruction;
+    int cycles = 0;
+
+    switch(opcode)
+    {
+        // Instruction: NOP
+        // Bytes: 1
+        // Cycles: 4
+        // Affected Registers: - - - -
+        case 0x00:
+            cycles = 1; 
+            break;
+    }
+
+    return cycles;
+}
