b467d8a7c831091b63d561eb014c99ce89ae37c4
Chandler Teigen <chandlerteigen@gmail.com>
2023-01-21 11:22:40 -0800

    add include guard to instruction_decoder.h

--- src/instruction_decoder.h
+++ src/instruction_decoder.h
@@ -3,8 +3,12 @@ Programmer: Chandler Teigen
 Date Created: 1/20/2023
 Date Modified: 1/20/2023
 */
+#ifndef REGISTERS_H
+#define REGISTERS_H
 
 #include "registers.h"
 
 
 int decode(char* p_instruction, registers *p_registers, char *p_memory);
+
+#endif
