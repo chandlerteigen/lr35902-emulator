fe8f0435859d5d47256de7c817ffd0160808e7e3
Chandler Teigen <chandlerteigen@gmail.com>
2023-01-26 19:29:27 -0800

    change register int types to uint types

--- src/registers.h
+++ src/registers.h
@@ -5,47 +5,47 @@ Date Modified: 1/20/2023
 */
 #ifndef REGISTERS_H
 #define REGISTERS_H
-#include <stdint.h>
+#include <stduint.h>
 
 typedef struct registers
 {
     union
     {
-        int16_t af;
+        uint16_t af;
         struct
         {
-            int8_t f;
-            int8_t a;
+            uint8_t f;
+            uint8_t a;
         };
     };
 
     union
     {
-        int16_t bc;
+        uint16_t bc;
         struct
         {
-            int8_t c;
-            int8_t b;
+            uint8_t c;
+            uint8_t b;
         };
     };
 
     union
     {
-        int16_t de;
+        uint16_t de;
         struct
         {
-            int8_t e;
-            int8_t d;
+            uint8_t e;
+            uint8_t d;
         };
     };
 
     union
     {
-        int16_t hl;
+        uint16_t hl;
         struct
         {
-            int8_t l;
-            int8_t h;
+            uint8_t l;
+            uint8_t h;
         };
     };
 } registers;
