a2e2e801fb1e871dcef1ff8b78f33c6d0db74888
Chandler Teigen <chandlerteigen@gmail.com>
2023-05-03 18:49:36 -0700

    add registers.h include to instruction_decoder.h

--- src/instruction_decoder.h
+++ src/instruction_decoder.h
@@ -6,7 +6,7 @@ Date Modified: 5/1/2023
 #ifndef INSTRUCTION_DECODER_H 
 #define INSTRUCTION_DECODER_H
 
-#include "registers.h"
+#include "registers.h"  
 
 
 int decode(char* const p_instruction, registers* const p_registers, char* const p_memory);
