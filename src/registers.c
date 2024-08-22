edd6d51c0da7b2419186fe141c9fb948f818c107
Chandler Teigen <chandlerteigen@gmail.com>
2024-08-22 16:48:27 -0700

    implement compute_add_carry function

--- src/registers.c
+++ src/registers.c
@@ -5,8 +5,9 @@ Date Modified: 8/22/2024
 */
 
 #include "registers.h"
+#include <math.h>
 
 bool compute_add_carry(int bit_position, uint32_t addend1, uint32_t addend2, uint32_t sum)
 {
-
+    return (addend1 ^ addend2 ^ sum) & (int)pow(2, bit_position + 1);
 }
