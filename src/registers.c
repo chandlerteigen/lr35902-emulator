7914cb47570fd94d66278b52c330343e77d0817c
Chandler Teigen <chandlerteigen@gmail.com>
2024-08-28 22:13:16 -0700

    implement set bit 16 bit

--- src/registers.c
+++ src/registers.c
@@ -21,7 +21,14 @@ void set_bit_8bit(uint8_t *dest, bool bit_val, int position)
 
 void set_bit_16bit(uint16_t *dest, bool bit_val, int position)
 {
-
+    if (bit_val)
+    {
+        *dest |= 1 << position;
+    }
+    else
+    {
+        *dest &= ~(1 << position);
+    }
 }
 
 bool compute_add_carry(int bit_position, uint32_t addend1, uint32_t addend2, uint32_t sum)
