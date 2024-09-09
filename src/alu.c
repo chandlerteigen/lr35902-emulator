5c94d64aa661b972bbc9425cbdd81ff366371007
Chandler Teigen <chandlerteigen@gmail.com>
2024-09-09 16:16:27 -0700

    fix add8 error where it was using the b register instead of addend

--- src/alu.c
+++ src/alu.c
@@ -5,13 +5,13 @@
 
 void add8(registers *p_registers, uint8_t addend)
 {
-    uint8_t sum = p_registers->a + p_registers->b;
+    uint8_t sum = p_registers->a + addend;
     uint8_t flags = 0;
     if (sum < p_registers->a)
     {
         flags = flags + C_FLAG_VAL;
     }
-    if (compute_add_carry(3, p_registers->a, p_registers->b, sum))
+    if (compute_add_carry(3, p_registers->a, addend, sum))
     {
         flags = flags + H_FLAG_VAL;
     }
